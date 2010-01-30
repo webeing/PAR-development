4de862e9317c1dee2c87730bfdf736b0f627a027
aghy@ivaio-windows <agata.cruciani@gmail.com>
2010-01-30 10:06:47 +0100

    featured.php

--- wp-content/themes/par-theme/includes/featured.php
+++ wp-content/themes/par-theme/includes/featured.php
@@ -10,7 +10,7 @@
 			 <?php $feature = new WP_Query('category_name='.get_option('woo_featured_category').'&showposts=1'); while ($feature->have_posts()) : $feature->the_post(); $do_not_duplicate = $post->ID; $preview = get_post_meta($post->ID, 'preview', true); ?>
 
 				<div class="featured-content">
-					<?php var_dump($blogsManager->getAllPostsByCategoryName('Featured')); ?>
+					<?php #var_dump($blogsManager->getAllPostsByCategoryName('Featured')); ?>
 					<h2 class="featured"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 					<?php the_excerpt(''); ?>
 				</div>
